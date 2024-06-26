3b0a0e91514b9f36a2475183c027c258acfc0fd0
shazia <160943899+dissconnect@users.noreply.github.com>
2024-06-26 02:24:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+let myBalance = 50000;
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "q1",
+        message: "enter your pin",
+        type: "number"
+    }
+]);
+// console.log(pinAnswer.q1);
+if (pinAnswer.pin === myPin) {
+    console.log("correct pin code!!!");
+    //     let operationAns = await inquirer.prompt(
+    //        [
+    //           {
+    //               name:"operation",
+    //               message:"please select option",
+    //                 type:"list",
+    //                 choices: ["withdraw","check balance"]
+    //             }
+    //         ]
+    //    )
+}
+//  else {
+//      console.log("incorrect pin number");
+// }
